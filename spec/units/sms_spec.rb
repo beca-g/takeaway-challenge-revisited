c15ec4229935fb116d919258a0f66f481a2fe3ec
Beca-g <rebekahgalliano@gmail.com>
2021-01-17 00:02:35 +0000

    SMS sends an estimated delivery time when an order is placed
    As a customer
    So that I can verify that my order is correct
    I would like to check that the total I have been given matches the sum of the various dishes in my order

--- spec/units/sms_spec.rb
+++ spec/units/sms_spec.rb
@@ -4,11 +4,19 @@ describe Sms do
   subject(:sms) { described_class.new(client) }
   let(:client) { double :client, messages: messages }
   let(:messages) { spy :messages }
+
+  let(:message) do
+    { 
+    body: "Thank you! Your order was placed and will be delivered no later than 18:52.",
+    from: nil,
+    to: nil } 
+  end
   
   describe "#send_sms" do
-    it "sends a SMS when the order has been placed" do
+    it "sends a SMS with an estimated delivery when the order has been placed" do
+      allow(Time).to receive(:now).and_return(Time.parse("17:52"))
       sms.send_sms
-      expect(messages).to have_received(:create)
+      expect(messages).to have_received(:create).with(message)
     end
   end
 end
